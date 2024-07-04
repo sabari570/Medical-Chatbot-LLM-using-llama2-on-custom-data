c8858ac4f8349d8304297cd8c29d2066603e04f8
sabari570 <rsabarisai2000@gmail.com>
2024-07-04 21:57:02 +0530

    updated store_index file

--- store_index.py
+++ store_index.py
@@ -1,28 +1,12 @@
 from src.helper import load_pdf, text_split, download_hugging_face_embeddings
-from langchain.vectorstores import Pinecone
-import pinecone
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
-
-# print(PINECONE_API_KEY)
-# print(PINECONE_API_ENV)
+from langchain_community.vectorstores import FAISS
 
 extracted_data = load_pdf("data/")
 text_chunks = text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 
-
-#Initializing the Pinecone
-pinecone.init(api_key=PINECONE_API_KEY,
-              environment=PINECONE_API_ENV)
-
-
-index_name="medical-bot"
-
 #Creating Embeddings for Each of The Text Chunks & storing
-docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
+db=FAISS.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
+
+# Saving the vector database locally
+db.save_local("saved_VDB")
